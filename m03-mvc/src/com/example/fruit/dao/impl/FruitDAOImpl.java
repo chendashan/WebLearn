365af5a7bff54688744af69b63773a9c71531b65
陈大山 <chendashan@tiange.com>
2022-06-30 11:17:31 +0800

    module03

---
+++ m03-mvc/src/com/example/fruit/dao/impl/FruitDAOImpl.java
@@ -0,0 +1,49 @@
+package com.example.fruit.dao.impl;
+
+import com.example.fruit.dao.FruitDAO;
+import com.example.fruit.pojo.Fruit;
+import com.example.myssm.basedao.BaseDAO;
+
+import java.util.List;
+
+public class FruitDAOImpl extends BaseDAO<Fruit> implements FruitDAO {
+
+    @Override
+    public List<Fruit> getFruitPageList(String keyword, Integer page) {
+        //SELECT * FROM t_fruit LIMIT (page - 1) * 5 , 5 ;
+        return super.executeQuery("select * from t_fruit where fname like ? or remark like ? limit ? , 5",
+                "%" + keyword + "%", "%" + keyword + "%", (page - 1) * 5);
+    }
+
+    @Override
+    public List<Fruit> getFruitList() {
+        return super.executeQuery("select * from t_fruit");
+    }
+
+    @Override
+    public Fruit getFruitByFid(Integer fid) {
+        return super.load("select * from t_fruit where fid = ?", fid);
+    }
+
+    @Override
+    public void updateFruit(Fruit fruit) {
+        String sql = "update t_fruit set fname = ? , price = ? , fcount = ? , remark = ? where fid = ?";
+        super.executeUpdate(sql, fruit.getFname(), fruit.getPrice(), fruit.getFcount(), fruit.getRemark(), fruit.getFid());
+    }
+
+    @Override
+    public void delFruit(Integer fid) {
+        super.executeUpdate("delete from t_fruit where fid = ? ", fid);
+    }
+
+    @Override
+    public void addFruit(Fruit fruit) {
+        super.executeUpdate("insert into t_fruit values(0, ?, ?, ?, ?)", fruit.getFname(), fruit.getPrice(), fruit.getFcount(), fruit.getRemark());
+    }
+
+    @Override
+    public int getFruitCount(String keyword) {
+        return ((Long) super.executeComplexQuery("select count(*) from t_fruit where fname like ? or remark like ?",
+                "%" + keyword + "%", "%" + keyword + "%")[0]).intValue();
+    }
+}
---
+++ m03-mvc/src/com/example/fruit/servlets/EditServlet.java
@@ -0,0 +1,30 @@
+package com.example.fruit.servlets;
+
+import com.example.fruit.dao.FruitDAO;
+import com.example.fruit.dao.impl.FruitDAOImpl;
+import com.example.fruit.pojo.Fruit;
+import com.example.myssm.myspringmvc.ViewBaseServlet;
+import com.example.myssm.util.StringUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/edit.do")
+public class EditServlet extends ViewBaseServlet {
+
+    private FruitDAO fruitDAO = new FruitDAOImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String fidStr = req.getParameter("fid");
+        if (StringUtil.isNotEmpty(fidStr)) {
+            int fid = Integer.parseInt(fidStr);
+            Fruit fruit = fruitDAO.getFruitByFid(fid);
+            req.setAttribute("fruit", fruit);
+            super.processTemplate("edit", req, resp);
+        }
+    }
+}
